9ec95c70682445732479a15d0996ff8a4a095560
DK <dkom@wisebits.com>
2022-05-02 22:15:29 +0300

    DOM last ones

--- lesson14/readme.md
+++ lesson14/readme.md
@@ -16,3 +16,55 @@
 - Обработчики событий. События contextmenu, change, input
 - Обработчики событий. Всплытие и погружение. Прекращение всплытия
 - Обработчики событий. Делегирование событий
+
+# Notes
+
+Получение элемента страницы
+- getElementById
+- getElementByClassName
+- getElementByTagName
+- querySelector
+- querySelectorAll
+
+Свойства элементов
+- className
+- id
+- innerText
+- innerHTML
+- textContent
+- children
+- dataset (data атрибуты)
+- style
+
+Создание элементов
+- createElement
+- append
+- prepend
+- insertAdjacentElement
+
+Методы элементов
+- remove
+- closest
+- classList
+- classList.add
+- classList.remove
+- classList.toggle
+- hasAttribute
+- getAttribute
+- removeAttribute
+- setAttribute
+
+События
+- click
+- submit (event.preventDefault())
+- keydown | keyup
+- mouseover | mouseout | mousemove
+- contextmenu
+- change
+- input
+
+Важные темы
+- Всплытие
+- погружение
+- делегирование Свойств
+
--- lesson14/task02/script.js
+++ lesson14/task02/script.js
@@ -1,6 +1,4 @@
-// --- Задание 2
-
-const tasks = [
+let tasks = [
   {
     id: "1138465078061",
     completed: false,
@@ -18,107 +16,171 @@ const tasks = [
   },
 ];
 
-function printMe() {
-    cleareTaskList();
-    
-    tasks.map((it) => {
-    
-      const checkboxForm_Checkbox = document.createElement('input');
-            checkboxForm_Checkbox.classList.add('checkbox-form__checkbox');
-            checkboxForm_Checkbox.setAttribute('type', 'checkbox');
-            checkboxForm_Checkbox.id = it.id;
-      const checkboxForm_label = document.createElement('label');
-            checkboxForm_label.htmlFor = it.id;
-    
-      const taskItem_Text = document.createElement('span');
-            taskItem_Text.textContent = it.text;
-      const checkboxForm = document.createElement('form');
-            checkboxForm.classList.add('checkbox-form');
-            checkboxForm.append(checkboxForm_Checkbox);
-            checkboxForm.append(checkboxForm_label);
-    
-      const taskItem_MainContent = document.createElement('div');
-            taskItem_MainContent.classList.add('task-item__main-content');
-            taskItem_MainContent.append(checkboxForm);
-            taskItem_MainContent.append(taskItem_Text);
-      const taskItem_DeleteBtn = document.createElement('button');
-            taskItem_DeleteBtn.classList.add('task-item__delete-button');
-            taskItem_DeleteBtn.classList.add('default-button');
-            taskItem_DeleteBtn.classList.add('delete-button');
-            taskItem_DeleteBtn.dataset.deleteTaskId = it.id;
-            taskItem_DeleteBtn.textContent = 'Удалить';
-    
-      const taskItem_MainContainer = document.createElement('div');
-            taskItem_MainContainer.classList.add('task-item__main-container');
-            taskItem_MainContainer.append(taskItem_MainContent);
-            taskItem_MainContainer.append(taskItem_DeleteBtn);
-    
-      const taskItem = document.createElement("div");
-            taskItem.classList.add("task-item");
-            taskItem.dataset.taskId = it.id;
-            taskItem.append(taskItem_MainContainer);
-      
-      const taskList = document.querySelector(".tasks-list");
-            taskList.append(taskItem);
-    });
+function render() {
+  removeTasks();
+
+  tasks.map((it) => {
+    const checkboxForm_Checkbox = document.createElement("input");
+    checkboxForm_Checkbox.classList.add("checkbox-form__checkbox");
+    checkboxForm_Checkbox.setAttribute("type", "checkbox");
+    checkboxForm_Checkbox.id = it.id;
+
+    const checkboxForm_label = document.createElement("label");
+    checkboxForm_label.htmlFor = it.id;
+
+    const taskItem_Text = document.createElement("span");
+    taskItem_Text.textContent = it.text;
+
+    const checkboxForm = document.createElement("form");
+    checkboxForm.classList.add("checkbox-form");
+    checkboxForm.append(checkboxForm_Checkbox);
+    checkboxForm.append(checkboxForm_label);
+
+    const taskItem_MainContent = document.createElement("div");
+    taskItem_MainContent.classList.add("task-item__main-content");
+    taskItem_MainContent.append(checkboxForm);
+    taskItem_MainContent.append(taskItem_Text);
+
+    const taskItem_DeleteBtn = document.createElement("button");
+    taskItem_DeleteBtn.classList.add("task-item__delete-button");
+    taskItem_DeleteBtn.classList.add("default-button");
+    taskItem_DeleteBtn.classList.add("delete-button");
+    taskItem_DeleteBtn.dataset.deleteTaskId = it.id;
+    taskItem_DeleteBtn.textContent = "Удалить";
+
+    const taskItem_MainContainer = document.createElement("div");
+    taskItem_MainContainer.classList.add("task-item__main-container");
+    taskItem_MainContainer.append(taskItem_MainContent);
+    taskItem_MainContainer.append(taskItem_DeleteBtn);
+
+    const taskItem = document.createElement("div");
+    taskItem.classList.add("task-item");
+    taskItem.dataset.taskId = it.id;
+    taskItem.append(taskItem_MainContainer);
+
+    const taskList = document.querySelector(".tasks-list");
+    taskList.append(taskItem);
+  });
 }
 
-printMe();
+render();
 
 // --- Задание 3
 
-const createTaskBlock = document.querySelector('.create-task-block');
-createTaskBlock.addEventListener('submit', (event) => {
+document
+  .querySelector(".create-task-block")
+  .addEventListener("submit", (event) => {
     event.preventDefault();
-    removeErrorMessage();
 
-    const text = event.target.querySelector('.create-task-block__input').value;
-    if (isEmpty(text)) {
-        showErrorMessage('Название задачи не должно быть пустым');
-    } 
-    else if (isDuplicate(text)) {
-        showErrorMessage('Задача с таким названием уже существует');
-    }
-    else {
-        tasks.push({
-            id: `${Date.now()}`,
-            completed: false,
-            text: text
-        });
-    }
-    
-    printMe();
-});
+    const { target } = event;
+    const textValue = target.querySelector(".create-task-block__input").value;
 
-// --- задание 4
+    if (isEmpty(textValue)) {
+      showErrorMessage("Название задачи не должно быть пустым");
+    } else if (isDuplicate(textValue)) {
+      showErrorMessage("Задача с таким названием уже существует");
+    } else {
+      hideErrorMessage();
+
+      tasks.push({
+        id: `${Date.now()}`,
+        completed: false,
+        text: textValue,
+      });
+    }
+
+    render();
+  });
 
 function isDuplicate(text) {
-    return tasks.findIndex(it => it.text === text) !== -1;
+  return tasks.findIndex((it) => it.text === text) !== -1;
 }
 
 function isEmpty(text) {
-    return text.length === 0;
+  return text.length === 0;
 }
+
+// --- задание 4
 
 function showErrorMessage(message) {
-    const errorMessageBlock = document.createElement('span');
-          errorMessageBlock.classList.add('error-message-block');
-          errorMessageBlock.textContent = message;
+  hideErrorMessage();
 
-    console.log('errorMessage', errorMessageBlock);
+  const errorMessage = document.createElement("span");
+  errorMessage.classList.add("error-message-block");
+  errorMessage.textContent = message;
 
-    const taskList = document.querySelector(".tasks-list");
-          taskList.parentNode.insertBefore(errorMessageBlock, taskList);
+  const target = document.querySelector(".tasks__input-wrapper");
+  target.append(errorMessage);
 }
 
-function removeErrorMessage() {
-    const errorMessageBlock = document.querySelector('.error-message-block');
-    if (errorMessageBlock) {
-        errorMessageBlock.remove();
-    }
+function hideErrorMessage() {
+  const errorMessage = document.querySelector(".error-message-block");
+  if (errorMessage) errorMessage.remove();
 }
 
-function cleareTaskList() {
-    const taskList = document.querySelector(".tasks-list");
-          taskList.innerHTML = '';
+function removeTasks() {
+  const tasksList = document.querySelector(".tasks-list");
+  tasksList.innerHTML = "";
+
+  const input = document.querySelector(".create-task-block__input");
+  input.value = "";
 }
+
+// --- задание 5
+
+const tasksList = document.querySelector(".tasks-list");
+tasksList.addEventListener("click", ({ target }) => {
+  if (target.classList.contains("task-item__delete-button")) {
+    showModal(target.dataset.deleteTaskId);
+  }
+});
+
+const showModal = (itemId) => {
+  const confirmBtn = document.createElement("button");
+  confirmBtn.classList.add("delete-modal__button");
+  confirmBtn.classList.add("delete-modal__confirm-button");
+  confirmBtn.textContent = "Удалить";
+  confirmBtn.addEventListener("click", () => {
+    tasks = tasks.filter((it) => it.id !== itemId);
+    render();
+    hideModal();
+  });
+
+  const cancelBtn = document.createElement("button");
+  cancelBtn.classList.add("delete-modal__button");
+  cancelBtn.classList.add("delete-modal__cancel-button");
+  cancelBtn.textContent = "Отмена";
+  cancelBtn.addEventListener("click", () => {
+    hideModal();
+  });
+
+  const deleteModal_Buttons = document.createElement("div");
+  deleteModal_Buttons.classList.add("delete-modal__buttons");
+  deleteModal_Buttons.append(cancelBtn);
+  deleteModal_Buttons.append(confirmBtn);
+
+  const deleteModal_Question = document.createElement("h3");
+  deleteModal_Question.classList.add("delete-modal__question");
+  deleteModal_Question.textContent =
+    "Вы действительно хотите удалить эту задачу?";
+
+  const deleteModal = document.createElement("div");
+  deleteModal.classList.add("delete-modal");
+  deleteModal.append(deleteModal_Question);
+  deleteModal.append(deleteModal_Buttons);
+
+  const modalOverlay = document.createElement("div");
+  modalOverlay.classList.add("modal-overlay");
+  modalOverlay.append(deleteModal);
+  modalOverlay.style.marginBottom = "1rem";
+
+  const target = document.querySelector("#tasks");
+  target.prepend(modalOverlay);
+};
+
+function hideModal() {
+  const modal = document.querySelector(".modal-overlay");
+  modal.innerHTML = "";
+}
+
+// --- задание 6
