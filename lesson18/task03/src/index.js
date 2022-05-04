46397ece6c181d974b8e0754586f089ad6a69f6a
DK <dkom@wisebits.com>
2022-05-04 23:24:24 +0300

    webpack task03

---
+++ lesson18/task03/src/index.js
@@ -0,0 +1,9 @@
+import "../index.css";
+import JS_IMAGE from '../assets/javaScript.png';
+
+console.log('hello world!');
+
+const img = document.createElement("img");
+img.src = JS_IMAGE;
+
+document.body.append(img);
